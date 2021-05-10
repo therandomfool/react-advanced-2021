dbcece8094264fef213d3b890c4a2a3b527a028e
therandomfool <mddenver@protonmail.com>
2021-05-09 17:19:10 -0700

    Set up

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'))
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(useState(value, handler))
+  const [text, setText] = useState('random title')
+
+  const handleClick = () => {
+    if (text === 'random title') {
+      setText('New Value')
+
+    } else {
+
+      setText('random title')
+    }
+  }
+  return <React.Fragment>
+    <h1>{text}</h1>
+    <button className='btn' onClick={handleClick}>Change Title</button>
+  </React.Fragment>
 };
 
 export default UseStateBasics;
